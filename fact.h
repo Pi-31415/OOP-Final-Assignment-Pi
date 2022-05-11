beb3c60e54fea8ed6a3c84ec2e7901e5a2116629
Pi <paingthetko.pi@gmail.com>
2022-05-11 20:28:58 +0400

    init

--- fact.h
+++
@@ -1,2 +0,0 @@
-// forward declaration
-int factorial(int);
--- factorial.cpp
+++
@@ -1,8 +0,0 @@
-
-// Recursive factorial program
-int factorial(int n) {
-  if (n == 1) {
-    return 1;
-  }
-  return n * factorial(n - 1);
-}
